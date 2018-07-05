da8210e1d1d6b5879a81eb58b165fc7042b8bc94
BadhanGanesh-HappyFox <ganesh.bj@happyfox.com>
2018-07-05 18:25:09 +0530

    Example project UI changed.

--- BJDraggableDemo/ViewController.swift
+++ BJDraggableDemo/ViewController.swift
@@ -9,26 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var draggableButton: UIButton!
-    @IBOutlet weak var draggableImageView: UIImageView!
+    
+    @IBOutlet weak var loginView: UIView!
+    @IBOutlet weak var omegaView: UIView!
+    @IBOutlet weak var omegaContainerView: UIView!
+    @IBOutlet weak var signupButton: UIButton!
+    
+    fileprivate func configureDraggabilities() {
+        self.omegaView.shouldResetViewPositionAfterRemovingDraggability = true
+        self.loginView.shouldResetViewPositionAfterRemovingDraggability = true
+        self.omegaContainerView.shouldResetViewPositionAfterRemovingDraggability = true
+        self.signupButton.shouldResetViewPositionAfterRemovingDraggability = true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.draggableButton.shouldResetViewPositionAfterRemovingDraggability = true
-        self.draggableImageView.shouldResetViewPositionAfterRemovingDraggability = true                
+        configureDraggabilities()
     }
     
     @IBAction func addDraggabilityButtonTouched(_ sender: UIButton) {
-        self.draggableButton.addDraggability(withinView: self.view, withMargin: .init(top: 40, left: 100, bottom: 20, right: 20))
-        self.draggableImageView.addDraggability(withinView: self.view)
+        self.loginView.addDraggability(withinView: self.view)
+        self.omegaView.addDraggability(withinView: self.omegaContainerView)
+        self.omegaContainerView.addDraggability(withinView: self.view)
+        self.signupButton.addDraggability(withinView: self.view)
     }
     
     @IBAction func removeDraggabilityButtonTouched(_ sender: UIButton) {
-        self.draggableButton.removeDraggability()
-        self.draggableImageView.removeDraggability()
+        self.loginView.removeDraggability()
+        self.omegaView.removeDraggability()
+        self.omegaContainerView.removeDraggability()
+        self.signupButton.removeDraggability()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
 }
 
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
+
+extension UITextField {
+    open override func awakeFromNib() {
+        self.layer.sublayerTransform = CATransform3DMakeTranslation(5, 0, 0)
+    }
+}
